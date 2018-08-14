1a7a1cea2b171cc54081f8b63b59a299a3c0680d
AlejandroAriasLomeli <42361660+AlejandroAriasLomeli@users.noreply.github.com>
2018-08-13 20:34:12 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  /*Enotnces esto es un comentario*/
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+ putchar('A');
+ putchar('Y');//Este es otro comentario
+ putchar('U');
+ putchar('D');
+ putchar('A');
+ putchar('\n');
+ printf("Esto aparece en mi texto \n");
+ printf("HOLA MUNDO \n");
+  system("PAUSE");	
+  return 0;
+}
